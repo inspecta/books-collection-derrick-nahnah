298d9e3eb3a2887c9e59f44e7de790440316e1cb
derrick <djmulinde17@gmail.com>
2022-09-19 21:05:56 +0300

    Display books dynamically from a JS Object

--- books.js
+++ books.js
@@ -1,4 +1,4 @@
-const books = [];
+let books = [];
 
 function Addbooks(author, title) {
   this.author = author;
@@ -6,29 +6,71 @@ function Addbooks(author, title) {
 }
 
 // let married = new Addbooks("Jimmy", "Main");
+
+
+const titleObj = document.getElementById('title')
+const authorObj = document.getElementById('author')
+const formObj = document.getElementById('form');
+// const add = document.getElementById('add');
+
 let married = {};
 
- const titleObj = document.getElementById('title')
- const authorObj = document.getElementById('author')
- const formObj = document.getElementById('form');
+formObj.addEventListener('submit', (event) => {
 
- formObj.addEventListener('submit', (event) => {
   event.preventDefault();
 
   married = new Addbooks(titleObj.value, authorObj.value);
-  // console.log(titleObj.value);
-  // console.log(authorObj.value);
+
   books.push(married);
-//  console.log(books)  
+  bookDetails();
 
- });
+  /*   const objectToString = JSON.stringify(books);
+    localStorage.setItem('xx', objectToString) */
 
-//  books.forEach() {
-//    document.createElement('div');
-//    document.createElement('p');
-//    document.createElement('p');
-//    document.createElement('button');
-//  }
+
+
+});
+
+console.log(books)
+/* const data = localStorage.getItem('xx');
+const dataParsed = JSON.parse(data);
+console.log(dataParsed) */
+
+const bookDetails = () => {
+  const booksDiv = document.getElementById('books-list')
+
+
+  books.forEach(i => {
+    //for (let i in books) {
+    const bookDiv = document.createElement('div');
+    bookDiv.setAttribute('id', 'book');
+    booksDiv.appendChild(bookDiv);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.id = 'bookAuthor';
+    bookDiv.appendChild(bookAuthor);
+    bookAuthor.appendChild(document.createTextNode(i.author))
+
+    const bookTitle = document.createElement('p');
+    bookTitle.id = 'bookTitle';
+    bookDiv.appendChild(bookTitle);
+    bookTitle.appendChild(document.createTextNode(i.title))
+
+    const removeBtn = document.createElement('button');
+    removeBtn.id = 'remove-btn';
+    removeBtn.className = 'remove-btn';
+    bookDiv.appendChild(removeBtn);
+    removeBtn.appendChild(document.createTextNode('Remove'))
+
+  });
+
+  // }
+}
+
+
+
+
+
 
 
 
