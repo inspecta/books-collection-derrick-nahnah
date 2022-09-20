5fb2e2dc06e3ddf5aef4cd9b67e225f3f96b6652
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-20 09:09:48 -0700

    Modify the books.js file

--- books.js
+++ books.js
@@ -7,16 +7,14 @@ function Addbooks(author, title) {
 
 // let married = new Addbooks("Jimmy", "Main");
 
-
-const titleObj = document.getElementById('title')
-const authorObj = document.getElementById('author')
-const formObj = document.getElementById('form');
+const titleObj = document.getElementById("title");
+const authorObj = document.getElementById("author");
+const formObj = document.getElementById("form");
 // const add = document.getElementById('add');
 
 let married = {};
 
-formObj.addEventListener('submit', (event) => {
-
+formObj.addEventListener("submit", (event) => {
   event.preventDefault();
 
   married = new Addbooks(titleObj.value, authorObj.value);
@@ -26,54 +24,58 @@ formObj.addEventListener('submit', (event) => {
 
   /*   const objectToString = JSON.stringify(books);
     localStorage.setItem('xx', objectToString) */
-
-
-
 });
 
-console.log(books)
+// console.log(books)
 /* const data = localStorage.getItem('xx');
 const dataParsed = JSON.parse(data);
 console.log(dataParsed) */
 
 const bookDetails = () => {
-  const booksDiv = document.getElementById('books-list')
+  const booksDiv = document.getElementById("books-list");
+  booksDiv.innerHTML = "";
+
+  const objectToString = JSON.stringify(books);
+  localStorage.setItem("xx", objectToString);
+
+  // console.log(books)
+  const data = localStorage.getItem("xx");
+  const dataParsed = JSON.parse(data);
+  console.log(dataParsed);
 
 
-  books.forEach(i => {
+  dataParsed.forEach((i) => {
     //for (let i in books) {
-    const bookDiv = document.createElement('div');
-    bookDiv.setAttribute('id', 'book');
+    const bookDiv = document.createElement("div");
+    bookDiv.setAttribute("id", "book");
     booksDiv.appendChild(bookDiv);
 
-    const bookAuthor = document.createElement('p');
-    bookAuthor.id = 'bookAuthor';
+    const bookAuthor = document.createElement("p");
+    bookAuthor.id = "bookAuthor";
     bookDiv.appendChild(bookAuthor);
-    bookAuthor.appendChild(document.createTextNode(i.author))
+    bookAuthor.appendChild(document.createTextNode(i.author));
 
-    const bookTitle = document.createElement('p');
-    bookTitle.id = 'bookTitle';
+    const bookTitle = document.createElement("p");
+    bookTitle.id = "bookTitle";
     bookDiv.appendChild(bookTitle);
-    bookTitle.appendChild(document.createTextNode(i.title))
+    bookTitle.appendChild(document.createTextNode(i.title));
 
-    const removeBtn = document.createElement('button');
-    removeBtn.id = 'remove-btn';
-    removeBtn.className = 'remove-btn';
+    const removeBtn = document.createElement("button");
+    // removeBtn.id = "remove-btn";
+    removeBtn.className = "remove-btn";
+    removeBtn.setAttribute('type', 'submit');
     bookDiv.appendChild(removeBtn);
-    removeBtn.appendChild(document.createTextNode('Remove'))
-
+    removeBtn.appendChild(document.createTextNode("Remove"));
+    // console.log(i)
+    const deleteOne =  document.querySelectorAll('.remove-btn')
+  deleteOne.forEach(i => {
+    i.addEventListener('click', () => {
+      // // dataParsed.remove('');
+      // console.log('btn-1')
+  })
+}) 
   });
 
   // }
-}
-
-
-
-
-
-
-
-
-
-
+};
 
