7ea65183834b6b13e2b5dbd67011a83f6a49d376
derrick <djmulinde17@gmail.com>
2022-09-22 12:44:46 +0300

    Add remove function, books array and objects in the class

--- books.js
+++ books.js
@@ -5,8 +5,6 @@ const container = document.getElementById('books-list');
 // eslint-disable-next-line no-unused-vars
 const reBtn = document.querySelector('.btn');
 
-let totalBooks = JSON.parse(localStorage.getItem('store')) || [];
-
 // BOOK CLASS CREATION
 class Books {
   constructor(title, author, id) {
@@ -15,26 +13,22 @@ class Books {
     this.id = id;
   }
 
+  static totalBooks = JSON.parse(localStorage.getItem('store')) || [];
+
   // eslint-disable-next-line class-methods-use-this
   displayBooks(bookObj) {
     const holder = document.createElement('div');
-    const bkTitle = document.createElement('h3');
-    const bkAuthor = document.createElement('p');
+    const titleAuthor = document.createElement('p');
     const removeBtn = document.createElement('button');
-    const line = document.createElement('hr');
 
-    // Assigning Innertext to Created elements
-
-    bkTitle.innerText = bookObj.title;
-    bkAuthor.innerText = bookObj.author;
-    removeBtn.innerText = 'Remove Book';
-    // Add object id to data-id attribute
+    titleAuthor.innerText = `"${bookObj.title}" by ${bookObj.author}`;
+    removeBtn.innerText = 'Remove';
     removeBtn.setAttribute('data-id', bookObj.id);
     removeBtn.className = 'btn';
-    holder.append(bkTitle);
-    holder.append(bkAuthor);
+    titleAuthor.className = 'title-author';
+    holder.className = 'book-holder';
+    holder.append(titleAuthor);
     holder.append(removeBtn);
-    holder.append(line);
     container.append(holder);
   }
 
@@ -44,8 +38,8 @@ class Books {
       // Retrieve data-id attribute.
       // It's a string, change it into number
       const id = Number(e.getAttribute('data-id'));
-      totalBooks = totalBooks.filter((book) => book.id !== id);
-      localStorage.setItem('store', JSON.stringify(totalBooks));
+      Books.totalBooks = Books.totalBooks.filter((book) => book.id !== id);
+      localStorage.setItem('store', JSON.stringify(Books.totalBooks));
       e.parentElement.remove();
     }
   }
@@ -54,7 +48,7 @@ class Books {
 const book = new Books();
 
 function loopFunc() {
-  totalBooks.forEach((e) => {
+  Books.totalBooks.forEach((e) => {
     book.displayBooks(e);
   });
 }
@@ -65,8 +59,8 @@ formObj.addEventListener('submit', (e) => {
   const id = Math.floor(Math.random() * 10000);
   const book = new Books(titleObj.value, authorObj.value, id);
   container.innerHTML = '';
-  totalBooks.push(book);
-  localStorage.setItem('store', JSON.stringify(totalBooks));
+  Books.totalBooks.push(book);
+  localStorage.setItem('store', JSON.stringify(Books.totalBooks));
   loopFunc();
   formObj.reset();
 });
